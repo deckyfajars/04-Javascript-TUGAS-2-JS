91def9ae321da0c0e8309c576955a512e77d9a01
Decky Fajar Sidiq <46802206+deckyfajars@users.noreply.github.com>
2021-07-01 17:08:05 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,16 @@
+var nomer = 18;
+var string = "18";
+
+var boolean1 = false;
+var booelan2 = 31;
+if (booelan2 > 30)
+	boolean1 = true;
+else
+	boolean1 = false;
+
+var nul = "test";
+nul = null;
+
+var undifin;
+
+console.log(undifin);
